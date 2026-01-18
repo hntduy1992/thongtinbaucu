3edbdc10161d977f378edc29e92fba74d8a13695
hntduy <hntduy1992@gmail.com>
2026-01-18 10:37:53 +0700

    next

--- source/resources/js/Components/Home/NguoiUngCu.vue
+++ source/resources/js/Components/Home/NguoiUngCu.vue
@@ -5,27 +5,53 @@ import UngCuView from "./UngCuView.vue";
 
 <template>
     <div class="content">
-        <v-carousel hide-delimiters class="bg-white">
-            <v-carousel-item class="bg-red carousel-item">
-                <v-container>
-                    <UngCuView></UngCuView>
-                </v-container>
+        <v-carousel hide-delimiters class="carousel" >
+            <v-carousel-item class="w-75  carousel-item">
+                <UngCuView></UngCuView>
+            </v-carousel-item>
+            <v-carousel-item class="w-75  carousel-item">
+                <UngCuView></UngCuView>
             </v-carousel-item>
         </v-carousel>
+        <v-list class="d-flex justify-center"  >
+            <v-list-item >
+                <div class="img-wrap">
+                    <img src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-hinh-the-dep-590x800.jpeg"></img>
+                </div>
+            </v-list-item>
+            <v-list-item >
+                <div class="img-wrap">
+                    <img src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-hinh-the-dep-590x800.jpeg"></img>
+                </div>
+            </v-list-item>
+        </v-list>
     </div>
 
 </template>
 
 <style scoped>
 .content{
-    background-color: red;
     height: 100%;
-
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: linear-gradient(10deg, #FFFAE8 10.67%, #FFE68D 64.88%, #FFDC9B 84.26%);
+}
+.carousel{
 }
 .carousel-item{
     display: flex;
     justify-content: center;
     align-items: center;
     margin: auto auto;
+}
+.img-wrap{
+    width: 100px;
+    height: 100px;
+    border: 3px solid  #A76700;
+}
+.v-list{
+  background-color: transparent;
+    margin-top: 50px;
 }
 </style>
--- source/resources/js/Components/Home/UngCuView.vue
+++ source/resources/js/Components/Home/UngCuView.vue
@@ -1,16 +1,73 @@
-<script setup lang="ts">
+<script setup>
 
 </script>
 
 <template>
-    <v-card>
-        <v-card-text>
-            Hình
-            Tên
-            Tuổi
+    <v-card class="wrap">
+        <v-card-title>
+            <h1 class=" text-center text-red">HỌ VÀ TÊN</h1>
+        </v-card-title>
+        <v-card-text class="content">
+            <div class="left-content">
+                <div class="img-wrap">
+                    <img style="width: 100%" class="avatar" src="../../../images/hinhdaibieu/cach-chup-hinh-the-dep-590x800.jpeg" alt="image">
+                </div>
+            </div>
+            <div class="right-content pl-5">
+                <p>Sinh ngày: <b>11/1/2021</b></p>
+                <p>Quê quán: <b>Sa Đéc, Đồng Tháp</b></p>
+                <p>Trình độ chuyên môn: <b>Thạc sỹ Quản lý kinh tế</b></p>
+                <p>Trình độ lý luận chính trị: <b>Cao cấp</b></p>
+            </div>
         </v-card-text>
     </v-card>
 </template>
 
 <style scoped>
+.wrap {
+    background-image: url("../../../images/TrongDong-bgcontent.png");
+    background-size: cover;
+
+    background-position: left center;
+    background-repeat: no-repeat;
+
+}
+.content{
+    display: flex;
+    justify-content: center;
+    min-height: calc(100%*3/4);
+    position: relative;
+    margin: 20px;
+
+}
+.content:after{
+    position: absolute;
+    content: '';
+    width: 3px;
+    height: 80%;
+    border-radius: 3px;
+    background-color: red;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+}
+.content .left-content,
+.content .right-content{
+    width: 50%;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+}
+.content .right-content{
+    font-size: 26pt;
+}
+.content .left-content{
+    align-items: center;
+}
+.content .left-content .img-wrap{
+    width: 350px;
+    height: 350px;
+}
 </style>
