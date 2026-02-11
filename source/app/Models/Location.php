fe686b585ab570b0da8c754256982fac34888c14
hntduy <hntduy1992@gmail.com>
2026-02-11 23:59:04 +0700

    fix giao dien

--- source/app/Models/Location.php
+++ source/app/Models/Location.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    protected $fillable = ['name', 'slug', 'file', 'img', 'latitude', 'longitude', 'phone', 'address', 'scope', 'region', 'qr', 'unit_id'];
+    protected $fillable = ['name', 'slug', 'file', 'img', 'latitude', 'longitude', 'phone', 'address', 'scope', 'region', 'qr', 'unit_id','place'];
 }
